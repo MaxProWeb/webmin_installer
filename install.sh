12d475206f9a0ac2d3d52f5b69188410be04624e
Ashwin Phatak <ashwinpphatak@gmail.com>
2011-01-02 01:18:32 -0800

    Updating for version 1.530

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 apt-get install libnet-ssleay-perl libauthen-pam-perl libio-pty-perl apt-show-versions libapt-pkg-perl
-wget http://prdownloads.sourceforge.net/webadmin/webmin_1.520_all.deb
-dpkg -i webmin_1.520_all.deb
+wget http://prdownloads.sourceforge.net/webadmin/webmin_1.530_all.deb
+dpkg -i webmin_1.530_all.deb
 
