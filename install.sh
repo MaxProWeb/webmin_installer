9ac26d3cc58389a76600bb60128a040fa880c40e
Max Yarchak <i@maxproweb.com>
2012-10-01 20:39:13 -0400

    added repository from webmin

--- install.sh
+++ install.sh
@@ -5,6 +5,12 @@ apt-get install -y libnet-ssleay-perl libauthen-pam-perl libio-pty-perl apt-show
 cd /root
 wget http://www.webmin.com/jcameron-key.asc
 apt-key add jcameron-key.asc
+
+echo '
+deb http://download.webmin.com/download/repository sarge contrib
+deb http://webmin.mirror.somersettechsolutions.co.uk/repository sarge contrib
+' >> /etc/apt/sources.list;
+
 apt-get update
 apt-get install webmin
 
