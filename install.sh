0ad00d797c0b08d019824ebb5bb16d76fa5872f1
Max Yarchak <i@maxproweb.com>
2012-10-01 20:27:21 -0400

    installing webmin via apt-get

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-apt-get install libnet-ssleay-perl libauthen-pam-perl libio-pty-perl apt-show-versions libapt-pkg-perl
-wget http://prdownloads.sourceforge.net/webadmin/webmin_1.530_all.deb
-dpkg -i webmin_1.530_all.deb
+apt-get update
+apt-get upgrade
+apt-get install -y libnet-ssleay-perl libauthen-pam-perl libio-pty-perl apt-show-versions libapt-pkg-perl
+cd /root
+wget http://www.webmin.com/jcameron-key.asc
+apt-key add jcameron-key.asc
+apt-get update
+apt-get install webmin
 
